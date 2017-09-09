5c3b459b0e83478596056a979504f239c94ffb22
niall mcginness <njwmcginness@gmail.com>
2017-09-08 17:51:23 -0700

    corrected location of static dir in main.go

--- backend/main.go
+++ backend/main.go
@@ -16,7 +16,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// serve static css and js files
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
 	http.HandleFunc("/", viewHandler)
 	http.ListenAndServe(PORT, nil)
 }
