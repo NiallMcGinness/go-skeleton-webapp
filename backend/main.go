d07704b68fdf79782b112e7c5c84a06652870cba
niall mcginness <njwmcginness@gmail.com>
2017-09-08 17:30:02 -0700

    added static folder to serve css and js

--- backend/main.go
+++ backend/main.go
@@ -1,22 +1,22 @@
-
-
 package main
 
 import (
-  "net/http"
-  "html/template"
+	"html/template"
+	"net/http"
 )
 
 const (
-  PORT = ":8080"
+	PORT = ":9000"
 )
 
-func viewHandler(w http.ResponseWriter, r *http.Request){
-	t, _ := template.ParseFiles("./frontend/html/main.html")
-	t.Execute(w,r)
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("../views/main.html")
+	t.Execute(w, r)
 }
 
 func main() {
-  http.HandleFunc("/", viewHandler)
-  http.ListenAndServe(PORT, nil)
+	// serve static css and js files
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", viewHandler)
+	http.ListenAndServe(PORT, nil)
 }
